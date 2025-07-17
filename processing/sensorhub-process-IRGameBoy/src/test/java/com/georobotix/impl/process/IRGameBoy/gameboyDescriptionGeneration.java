31465819f3984a491e54042881c08669ab910cf9
Jorge Mares <kokomares@gmail.com>
2025-07-17 16:21:54 -0600

    new xml description generation

---
+++ processing/sensorhub-process-IRGameBoy/src/test/java/com/georobotix/impl/process/IRGameBoy/gameboyDescriptionGeneration.java
@@ -0,0 +1,38 @@
+package com.georobotix.impl.process.IRGameBoy;
+
+import com.botts.process.helpers.ProcessHelper;
+import com.google.errorprone.annotations.RestrictedApi;
+import net.opengis.sensorml.v20.AggregateProcess;
+import org.junit.Test;
+import org.vast.data.SWEFactory;
+import org.vast.process.ProcessException;
+import org.vast.xml.XMLWriterException;
+
+public class gameboyDescriptionGeneration {
+    SWEFactory fac = new SWEFactory();
+    ProcessHelper processHelper = new ProcessHelper();
+
+    public AggregateProcess generateDescription() throws ProcessException {
+        GameboyProcess p1 = new GameboyProcess();
+        p1.init();
+
+        return processHelper.createProcessChain()
+                .name("gameboycamera")
+                .uid("urn:osh:process:gameboycamera")
+                .description("generic description duh")
+                .addDataSource("joyconcamera", "urn:osh:sensor:joycon-image")
+                .addOutputList(p1.getOutputList())
+                .addProcess("gameboyprocess", p1)
+                .addConnection("components/joyconcamera/outputs/joyconIROutput",
+                        "components/gameboyprocess/inputs/VideoFrame")
+                .addConnection("components/gameboyprocess/outputs/VideoFrame",
+                        "outputs/VideoFrame")
+                .build();
+    }
+
+    @Test
+    public void generateDescXML() throws ProcessException, XMLWriterException {
+        processHelper.writeProcess(System.out, generateDescription(), true);
+    }
+
+}
